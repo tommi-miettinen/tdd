03c5f85d38bd95e840b9824a213f7c9700d9d71c
unknown <tomppa.miettinen1@gmail.com>
2024-01-21 01:07:18 +0200

    create helper methods

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -23,54 +23,53 @@ export class Shop {
     this.items = items;
   }
 
+  decreaseQuality(item) {
+    if (!item.degradeable) return;
+
+    if (item.quality > 0) {
+      item.quality = item.quality - 1;
+    }
+  }
+
+  increaseQuality(item) {
+    if (item.quality < 50) {
+      item.quality = item.quality + 1;
+    }
+  }
+
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
       if (
-        this.items[i].quality > 0 &&
         this.items[i].degradeable &&
         this.items[i].name != "Aged Brie" &&
         this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
       ) {
-        this.items[i].quality = this.items[i].quality - 1;
+        this.decreaseQuality(this.items[i]);
       } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
+        this.increaseQuality(this.items[i]);
 
-          if (
-            this.items[i].quality < 50 &&
-            this.items[i].name == "Backstage passes to a TAFKAL80ETC concert" &&
-            this.items[i].sellIn < 11
-          ) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
+        if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert" && this.items[i].sellIn < 11) {
+          this.increaseQuality(this.items[i]);
+        }
 
-          if (
-            this.items[i].sellIn < 6 &&
-            this.items[i].quality < 50 &&
-            this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
-          ) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
+        if (this.items[i].sellIn < 6 && this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+          this.increaseQuality(this.items[i]);
         }
       }
       if (this.items[i].needsToBeSold) {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
 
-      if (
-        this.items[i].quality > 0 &&
-        this.items[i].degradeable &&
-        this.items[i].name != "Age Brie" &&
-        this.items[i].sellIn < 0
-      ) {
-        this.items[i].quality = this.items[i].quality - 1;
+      if (this.items[i].sellIn < 0) {
+        this.decreaseQuality(this.items[i]);
       }
+
       if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert" && this.items[i].sellIn < 0) {
         this.items[i].quality = 0;
       }
 
-      if (this.items[i].quality < 50 && this.items[i].name == "Aged Brie" && this.items[i].sellIn < 0) {
-        this.items[i].quality = this.items[i].quality + 1;
+      if (this.items[i].name == "Aged Brie" && this.items[i].sellIn < 0) {
+        this.increaseQuality(this.items[i]);
       }
     }
 
